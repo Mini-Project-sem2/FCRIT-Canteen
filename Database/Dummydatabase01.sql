5c3ce80e52c0cb5261a25ab9ca1ff1709f1aae46
Farhan-hyd <mfarhanhyd5@gmail.com>
2021-02-10 12:17:45 +0530

    updated

---
+++ Database/Dummydatabase01.sql
@@ -0,0 +1,14 @@
+CREATE TABLE students (id INTEGER PRIMARY KEY,student_name TEXT,roll_no INTEGER ,password password);
+
+INSERT INTO students (student_name,roll_no,password) VALUES( "Aditiya U",1019101,"password@121");
+INSERT INTO students (student_name,roll_no,password) VALUES( "Rishika A",1019102,"password@122");
+INSERT INTO students (student_name,roll_no,password) VALUES( "Cristopher J",1019103,"password@123");
+INSERT INTO students (student_name,roll_no,password) VALUES( "Anushka P",1019104,"password@124");
+INSERT INTO students (student_name,roll_no,password) VALUES( "Anna J",1019105,"password@125");
+INSERT INTO students (student_name,roll_no,password) VALUES( "Antony B",1019106,"password@126");
+INSERT INTO students (student_name,roll_no,password) VALUES( "DIVYA K",1019107,"password@127");
+INSERT INTO students (student_name,roll_no,password) VALUES( "Najuka H",1019108,"password@128");
+INSERT INTO students (student_name,roll_no,password) VALUES( "Bryan F",1019109,"password@129");
+INSERT INTO students (student_name,roll_no,password) VALUES( "Rachel P",1019110,"password@120");
+
+SELECT*FROM students
