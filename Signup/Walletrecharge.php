320e1cffea40d4a5f24a6309e1d5ff215c221ce5
Farhan-hyd <mfarhanhyd5@gmail.com>
2021-04-11 20:59:16 +0530

    u 11-04

--- Signup/Walletrecharge.php
+++ Signup/Walletrecharge.php
@@ -5,50 +5,42 @@ $password = "";
 $dbname = "canteendb";
 $conn = mysqli_connect("localhost", "root", "", "canteendb");
 $error = ''; // Variable To Store Error Message
-if (isset($_POST['submit'])) { 
-   if (empty($_POST['update']) ||empty($_POST['fname']) || empty($_POST['price'])) {
-      $error = "form is incomplete";
-   }else{
-      // Define $username and $password
-      $action = $_POST['update'];
-      $fname = $_POST['fname'];
-      $price = $_POST['price'];
-
-      if($action == 'add'){
-        $query="INSERT INTO MENU(Fname,Price) VALUES('".$fname."',".$price.")";
+if (isset($_POST['submit'])) {
+    if (empty($_POST['username']) || empty($_POST['amount'])) {
+        $error = "form is incomplete";
+    }else {
+        $username = $_POST['username'];
+        $amount = $_POST['amount'];
+        $query = "UPDATE costumers SET Balance=Balance+$amount WHERE username='$username'";
         $query_run = mysqli_query($conn, $query);
-        if (!$query_run) {
+       if(!$query_run) {
           printf("Error: %s\n", mysqli_error($conn));
           exit();
         }
-      }else{
-        $query='DEELETE FROM MENU WHERE Fname=$fname';
-        $query_run = mysqli_query($conn, $query);
-        if (!$query_run) {
-          printf("Error: %s\n", mysqli_error($conn));
-          exit();
-        }
-      }
-   }   
-}
+    }
+} 
 ?>
 
 <DOCTYPE html>
-  <html lang="en">
-
+<html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>FCRIT Canteen | Login</title>
     <link rel="icon" href="css/fcritlogo.png">
-    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link
+      href="https://fonts.googleapis.com/css?family=Raleway"
+      rel="stylesheet"
+    />
+    <link 
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" 
+      rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" 
+      crossorigin="anonymous">
     <style>
       * {
         box-sizing: border-box;
-        margin: 0;
+        margin:0;
         padding: 0;
       }
 
@@ -121,37 +113,26 @@ if (isset($_POST['submit'])) {
       }
 
       footer a {
-        color: #49c1a2
+        color:#49c1a2
       }
     </style>
   </head>
-
   <body>
     <div id="container">
       <div class="form-wrap">
-        <h1>UPDATE MENU</h1>
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-          <div class="form-group">
-            <label for="update" id="update">UPDATE</label>
-            <div>
-              <input type="radio" id="add" name="update" value="add">
-              <label for="add">ADD</label>
-              <input type="radio" id="delete" name="update" value="delete">
-              <label for="delete">DELETE</label>
-            </div>
+        <h1>Login</h1>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+          <div class="form-group" >
+            <label for="username" id="username">Username</label>
+            <input type="text" name="username" id="username" />
           </div>
           <div class="form-group">
-            <label for="fname" id="fname">ITEM NAME</label>
-            <input type="text" name="fname" id="fname" />
+            <label for="amount" id="amount">Amount</label>
+            <input type="number" name="amount" id="amount" />
           </div>
-          <div class="form-group">
-            <label for="price" id="price">Price</label>
-            <input type="price" name="price" id="price" />
-          </div>
-          <button type="submit" class="btn" name="submit">UPDATE</button>
+          <button type="submit" class="btn" name="submit">RECHARGE</button>
         </form>
       </div>
     </div>
   </body>
-
-  </html>
+</html>
